2fca8b9c4c77f10163fcf145de487765fa716231
vubinh127 <81604514+vubinh127@users.noreply.github.com>
2022-06-02 23:02:58 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
+jQuery(document).ready(function() {
+    "use strict"
+
+    $(".body").ripples({ //function to get the  ripple water effect 
+        dropRadius: 12,
+        perturbance: 0.01,
+
+    });
+});
+
 document.addEventListener('DOMContentLoaded', function() {
-  
-  var days = document.querySelector('.days span');
-  var hour = document.querySelector('.hour');
-  var min = document.querySelector('.min');
-  var second = document.querySelector('.second');
 
-  var startDate = new Date(2021, 2, 20);
-  days.innerText = Math.floor((new Date - startDate)/86400000);
-  countTime();
+    var days = document.querySelector('.days span');
+    var hour = document.querySelector('.hour');
+    var min = document.querySelector('.min');
+    var second = document.querySelector('.second');
 
-  function countTime() {
-    let today = new Date();
-    let ms = (today - startDate) % 86400000;
-    hour.innerText = Math.floor(ms / 3600000);
-    min.innerText = Math.floor(ms % 3600000 / 60000);
-    second.innerText = Math.floor(ms % 3600000 % 60000 / 1000);
-  }
+    var startDate = new Date(2021, 2, 20);
+    days.innerText = Math.floor((new Date - startDate) / 86400000);
+    countTime();
 
-  setInterval(countTime, 1000);
+    function countTime() {
+        let today = new Date();
+        let ms = (today - startDate) % 86400000;
+        hour.innerText = Math.floor(ms / 3600000);
+        min.innerText = Math.floor(ms % 3600000 / 60000);
+        second.innerText = Math.floor(ms % 3600000 % 60000 / 1000);
+    }
+
+    setInterval(countTime, 1000);
 
 }, false);
